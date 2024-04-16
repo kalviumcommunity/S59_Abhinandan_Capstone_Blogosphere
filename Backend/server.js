0569670a54a2ee93997879e54f37204b6b7254c2
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-04-16 17:30:25 +0530

    made the registration post in front end

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv'); 
 dotenv.config();
 const app = express();
+const cors = require('cors')
 const routes = require("./Controllers/routes");
 const userRoutes = require("./Controllers/User-Routes")
 app.use(express.json());
+app.use(cors())
 app.use('/data', routes);
 app.use('/user', userRoutes)
 const port = process.env.PORT;
