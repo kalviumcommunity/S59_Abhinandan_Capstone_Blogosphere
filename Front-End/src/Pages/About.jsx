62cdf874fe1badb39d08f5f21b3cadb770a48074
ananyatewari <ananyatewari0205@gmail.com>
2024-06-23 12:23:12 +0530

    flowbite react installed with some trial components

--- Front-End/src/Pages/About.jsx
+++ Front-End/src/Pages/About.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import SideNavBarClose from '../Components/SideNavBarClose';
+import { Button, Checkbox, Label, TextInput, Blockquote } from 'flowbite-react';
 
 function About() {
   return (
-    <div>  
-        <div className='flex justify-center items-center ' style={{ height: "calc(100vh - 8vw)", overflowY: "scroll", width: "100vw" }}>
-          <div className='bg-blue-500 p-4 rounded text-gray-700 font-bold font-sans'>About</div>
+    <div>
+      <Navbar />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div>
+          <SideNavBarClose />
         </div>
+
+        <div className='flex justify-center items-center' style={{ height: "calc(100vh - 8vw)", width: "100vw", margin: "4vh 10vh 4vh 10vh" }}>
+          {/* <div className='bg-blue-500 p-4 rounded text-gray-700 font-bold font-sans'>About</div> */}
+          <div className='flex flex-col justify-center items-center'>
+
+          <Blockquote className="my-4 border-l-4 border-gray-300 bg-gray-100 p-4 dark:border-gray-500 dark:bg-gray-800 mb-10 w-[50vw]">
+            "At Blogosphere, we believe in the power of ideas to inspire change. Join us on a journey of discovery, empowerment, and innovation as we explore the world together with words !"
+          </Blockquote>
+
+          <form className="flex flex-col gap-6 w-full max-w-md p-8 border border-gray-200 rounded-lg shadow-lg bg-white">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email1" value="Your email" className="text-lg font-semibold text-gray-700" />
+              </div>
+              <TextInput id="email1" type="email" placeholder="name@blogosphere.com" required className="p-2 border-none focus:ring-2 focus:ring-blue-500 rounded-md w-full" />
+            </div>
+
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="password1" value="Your password" className="text-lg font-semibold text-gray-700" />
+              </div>
+              <TextInput id="password1" type="password" required className="p-2 border-none focus:ring-2 focus:ring-blue-500 rounded-md w-full" />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Checkbox id="remember" className="rounded" />
+              <Label htmlFor="remember" className="text-gray-600">Remember me</Label>
+            </div>
+
+            <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md">
+              Submit
+            </Button>
+          </form>
+
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
